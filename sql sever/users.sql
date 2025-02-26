f42c0a57d1121aae672c25fe08c24e59f27bc38d
fateme529 <fdamavandi529@gmail.com>
2025-02-26 14:51:40 +0330

    v0.0.3

---
+++ sql sever/users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INT IDENTITY PRIMARY KEY,
+    username NVARCHAR(100) UNIQUE NOT NULL,
+    passwordhash NVARCHAR(256) NOT NULL,
+    walletaddress NVARCHAR(100) NOT NULL,
+    balance DECIMAL(25,2) DEFAULT 0
+)
